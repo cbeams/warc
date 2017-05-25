0d9cb5291d6d29112adb42b6be6d8945289aa34a
Chris Beams <chris@beams.io>
2017-05-25 14:44:59 +0200

    Make OptionalReader implement Iterable<T> as well

--- src/main/java/org/iokit/core/read/OptionalReader.java
+++ src/main/java/org/iokit/core/read/OptionalReader.java
@@ -2,6 +2,8 @@ package org.iokit.core.read;
 
 import java.io.InputStream;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -9,7 +11,7 @@ import java.util.stream.StreamSupport;
 import static java.util.Spliterator.*;
 import static java.util.Spliterators.spliteratorUnknownSize;
 
-public abstract class OptionalReader<T> extends Reader<T> {
+public abstract class OptionalReader<T> extends Reader<T> implements Iterable<T> {
 
     public OptionalReader(InputStream in) {
         super(in);
@@ -22,8 +24,37 @@ public abstract class OptionalReader<T> extends Reader<T> {
     public abstract Optional<T> readOptional() throws ReaderException;
 
     public Stream<T> stream() {
-        return StreamSupport.stream(
-            spliteratorUnknownSize(new OptionalReaderIterator<>(this), NONNULL | ORDERED | IMMUTABLE),
-            false);
+        return StreamSupport.stream(spliteratorUnknownSize(iterator(), NONNULL | ORDERED | IMMUTABLE), false);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private T nextValue = null;
+
+            @Override
+            public boolean hasNext() {
+                if (nextValue != null)
+                    return true;
+
+                Optional<T> result = OptionalReader.this.readOptional();
+                if (result.isPresent()) {
+                    nextValue = result.get();
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public T next() {
+                if (nextValue != null || hasNext()) {
+                    T value = nextValue;
+                    nextValue = null;
+                    return value;
+                }
+                throw new NoSuchElementException();
+            }
+        };
     }
 }
