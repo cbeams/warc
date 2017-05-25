89e2a87cc1a4516346c7a1a579feecde74c0fa4a
Chris Beams <chris@beams.io>
2017-05-25 12:19:08 +0200

    Add missing enum labels to WarcDefinedField

--- src/main/java/org/iokit/warc/WarcDefinedField.java
+++ src/main/java/org/iokit/warc/WarcDefinedField.java
@@ -4,16 +4,34 @@ import org.iokit.imf.DefinedField;
 import org.iokit.imf.FieldName;
 
 public enum WarcDefinedField implements DefinedField { // TODO: support field-type specific validation
-    WARC_Type("WARC-Type"),
-    WARC_Record_ID("WARC-Record-ID"),
-    Content_Type("Content-Type"),
-    Content_Length("Content-Length"),
-    WARC_Date("WARC-Date");
+    WARC_Type,
+    WARC_Record_ID,
+    WARC_Date,
+    Content_Length,
+    Content_Type,
+    WARC_Concurrent_To,
+    WARC_Block_Digest,
+    WARC_Payload_Digest,
+    WARC_IP_Address,
+    WARC_Refers_To,
+    WARC_Target_URI,
+    WARC_Truncated,
+    WARC_Warcinfo_ID,
+    WARC_Filename,
+    WARC_Profile,
+    WARC_Identified_Payload_Type,
+    WARC_Segment_Origin_ID,
+    WARC_Segment_Number,
+    WARC_Segment_Total_Length;
 
     private final FieldName fieldName;
 
-    WarcDefinedField(String fieldName) {
-        this.fieldName = new FieldName(fieldName);
+    WarcDefinedField() {
+        this.fieldName = new FieldName(translate(name()));
+    }
+
+    private String translate(String name) {
+        return name.replace('_', '-');
     }
 
     @Override
