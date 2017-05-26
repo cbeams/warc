a36e91e14c7c12f3c2b1def2124182ee7c135819
Chris Beams <chris@beams.io>
2017-05-26 10:30:13 +0200

    Add LineWriterSpec

---
+++ src/test/java/org/iokit/line/LineWriterSpec.java
@@ -0,0 +1,25 @@
+package org.iokit.line;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.iokit.line.LineTerminator.*;
+
+public class LineWriterSpec {
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Test
+    public void writeLineTerminatedByCrlf() {
+        new LineWriter(out, CR_LF).write();
+        assertThat(out.toByteArray()).isEqualTo(CR_LF.bytes);
+    }
+
+    @Test
+    public void writeLineTerminatedByLf() {
+        new LineWriter(out, LF).write();
+        assertThat(out.toByteArray()).isEqualTo(LF.bytes);
+    }
+}
