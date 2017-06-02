90d98edd860749b64f4c1f9de103a044c79ec39e
Chris Beams <chris@beams.io>
2017-06-02 11:45:43 +0200

    Split warc and warcbench repositories
    This change removes performance tests that rely on actual warc files
    from the Common Crawl dataset. These tests will be moved into the
    forthcoming https://github.com/cbeams/warcbench repository, which will
    use Git LFS to pull in these large files and execute often
    long-running tests against them.
    
    Also removed are dependencies on other JVM-based warc implementations
    like webarchive-commons, JWAT and bubing. Part of the intention of the
    separate warcbench repository is to compare performance among these
    different implementations.

--- src/test/java/io/webgraph/warc/WarcWriterSpec.java
+++ src/test/java/io/webgraph/warc/WarcWriterSpec.java
@@ -1,6 +1,5 @@
 package io.webgraph.warc;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.zip.GZIPInputStream;
@@ -71,24 +70,5 @@ public class WarcWriterSpec {
         }
 
         assertThat(newFile).hasSameContentAs(oldFile);
-    }
-
-    @Test
-    @Ignore
-    public void copyEntireWarcFile() throws IOException {
-        File oldFile = new File("/Users/cbeams/Work/webgraph/data/commoncrawl/crawl-data/CC-MAIN-2017-13/segments/1490218186353.38/wat/CC-MAIN-20170322212946-00003-ip-10-233-31-227.ec2.internal.warc.wat.gz");
-        File newFile = new File("/tmp/00003.warc.gz");
-
-        // 42864 (before parallel)
-        // 18797 (after parallel)
-        // 68630 (after segmentable)
-
-        try (Warc.Reader reader = new Warc.Reader(oldFile);
-             Warc.Writer writer = new Warc.Writer(newFile)) {
-
-            reader.stream().forEach(writer::write);
-        }
-
-        //assertThat(newFile).hasSameContentAs(originalFile);
     }
 }
